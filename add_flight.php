76257df6b6c6e947a0ba2f45a2051d32d310e674
anuari123 <anuarkiqmari1@gmail.com>
2025-12-17 16:34:43 +0100

    Update add_flight.php

--- add_flight.php
+++ add_flight.php
@@ -1,12 +1,27 @@
 <?php
-include 'config.php';
+include "config.php";
 
-
-if(isset($_POST['add'])){
-$stmt = $conn->prepare("INSERT INTO flights (origin, destination, flight_date, price) VALUES (?, ?, ?, ?)");
-$stmt->execute([ $_POST['origin'], $_POST['destination'], $_POST['date'], $_POST['price'] ]);
-
-
-header("Location: dashboard.php");
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $stmt = $pdo->prepare(
+        "INSERT INTO flights (origin,destination,flight_date,price)
+         VALUES (?,?,?,?)"
+    );
+    $stmt->execute([
+        $_POST["origin"],
+        $_POST["destination"],
+        $_POST["date"],
+        $_POST["price"]
+    ]);
+    header("Location: dashboard.php");
 }
 ?>
+
+<h2>Add Flight</h2>
+
+<form method="POST">
+    <input name="origin" placeholder="From"><br>
+    <input name="destination" placeholder="To"><br>
+    <input type="date" name="date"><br>
+    <input type="number" name="price"><br>
+    <button>Add</button>
+</form>
